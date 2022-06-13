31cd1195e5f64f156e9b46e895d4832d68fe4b8d
Rui Lopes <rgl@ruilopes.com>
2022-06-13 19:38:21 +0100

    install ansible builder 1.1.0

---
+++ provision-ansible-builder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+# see https://pypi.org/project/ansible-builder/
+# see https://ansible-builder.readthedocs.io/en/stable/
+ansible_builder_version='1.1.0'
+
+# install.
+apt-get install -y --no-install-recommends python3-pip
+python3 -m pip install ansible-builder==$ansible_builder_version
