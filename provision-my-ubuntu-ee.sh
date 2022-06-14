3cfcd1a6a35c73d526fa3ac4859261fcf35fc10a
Rui Lopes <rgl@ruilopes.com>
2022-06-14 09:08:53 +0100

    show information about the built execution environment

--- provision-my-ubuntu-ee.sh
+++ provision-my-ubuntu-ee.sh
@@ -5,6 +5,7 @@ if [ ! -d my-ubuntu-ansible-playbooks ]; then
   git clone https://github.com/rgl/my-ubuntu-ansible-playbooks.git
 fi
 
+# build.
 cd my-ubuntu-ansible-playbooks
 cat >execution-environment.yml <<'EOF'
 version: 1
@@ -16,3 +17,14 @@ dependencies:
   python: requirements.txt
 EOF
 ansible-builder build --verbosity 3 --tag my-ubuntu-ee
+
+# show information about the built execution environment.
+# see https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#run
+kubectl run my-ubuntu-ee-info -q -i --rm --restart=Never --image-pull-policy=Never --image=my-ubuntu-ee -- bash <<'EOF'
+exec 2>&1
+set -euxo pipefail
+cat /etc/os-release
+ansible --version
+python3 -m pip list
+ansible-galaxy collection list
+EOF
--- provision-my-windows-ee.sh
+++ provision-my-windows-ee.sh
@@ -5,6 +5,7 @@ if [ ! -d my-windows-ansible-playbooks ]; then
   git clone https://github.com/rgl/my-windows-ansible-playbooks.git
 fi
 
+# build.
 cd my-windows-ansible-playbooks
 cat >execution-environment.yml <<'EOF'
 version: 1
@@ -16,3 +17,14 @@ dependencies:
   python: requirements.txt
 EOF
 ansible-builder build --verbosity 3 --tag my-windows-ee
+
+# show information about the built execution environment.
+# see https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#run
+kubectl run my-windows-ee-info -q -i --rm --restart=Never --image-pull-policy=Never --image=my-windows-ee -- bash <<'EOF'
+exec 2>&1
+set -euxo pipefail
+cat /etc/os-release
+ansible --version
+python3 -m pip list
+ansible-galaxy collection list
+EOF
