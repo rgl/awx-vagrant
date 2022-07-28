0c8c362d43b06d8ee91d41527a6adafad5a58cee
Rui Lopes <rgl@ruilopes.com>
2022-07-28 19:42:25 +0100

    remove the ansible-lint dependency
    this dependency is not working with the ansible-builder base image and its not really required for my use-case

--- provision-my-ubuntu-ee.sh
+++ provision-my-ubuntu-ee.sh
@@ -7,6 +7,14 @@ fi
 
 # build.
 cd my-ubuntu-ansible-playbooks
+# remove the ansible-lint dependency.
+# NB without this, the execution environment build fails with:
+#     ERROR: Cannot install -r /tmp/src/requirements.txt (line 10) and jsonschema==3.2.0 because these package versions have conflicting dependencies.
+#     The conflict is caused by:
+#       The user requested jsonschema==3.2.0
+#       ansible-lint 6.3.0 depends on jsonschema>=4.6.0
+sed -i -E '/^ansible-lint=.+/d' requirements.txt
+# define the execution environment.
 cat >execution-environment.yml <<'EOF'
 version: 1
 build_arg_defaults:
@@ -16,6 +24,7 @@ dependencies:
   galaxy: requirements.yml
   python: requirements.txt
 EOF
+# build the execution environment.
 ansible-builder build --verbosity 3 --tag my-ubuntu-ee
 
 # show information about the built execution environment.
